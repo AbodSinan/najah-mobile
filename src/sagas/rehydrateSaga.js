7a81928e911962d71fb42e2fb9197ac8ee844268
Sinan <abodsinan96@gmail.com>
2022-05-08 00:29:31 +0800

    Added fetching data when refreshing

---
+++ src/sagas/rehydrateSaga.js
@@ -0,0 +1,26 @@
+import { REHYDRATE } from "redux-persist/lib/constants";
+import { takeEvery, put, all, select } from "redux-saga/effects";
+
+import { getUserToken } from "./selectors";
+import api from "../services/api/Api";
+
+function* handleRehydrate() {
+  const token = yield select(getUserToken);
+  api.setToken(token);
+
+  yield put(api.getSubjects.createAction());
+  yield put(api.getClasses.createAction());
+  yield put(api.getUserClasses.createAction());
+  yield put(api.getUserPrivateClasses.createAction());
+  yield put(api.getEducationLevels.createAction());
+  yield put(api.getSubjectCategories.createAction());
+  yield put(api.getPrivateClasses.createAction());
+}
+
+function* watchRehydrate() {
+  yield takeEvery(REHYDRATE, handleRehydrate);
+}
+
+export default function* rehydrateSaga() {
+  yield all([watchRehydrate()]);
+}
--- src/sagas/rootsaga.js
+++ src/sagas/rootsaga.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import userSaga from "./userSaga";
+import rehydrateSaga from "./rehydrateSaga";
 
 export default function* rootSaga() {
-  yield all([userSaga()]);
+  yield all([userSaga(), rehydrateSaga()]);
 }
